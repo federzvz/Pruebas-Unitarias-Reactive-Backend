6bea35da7d25ddc64e33468fab529f69ff7b09fd
federzvz <85709865+federzvz@users.noreply.github.com>
2022-03-19 16:10:59 -0300

    Finished

---
+++ src/test/java/co/com/sofka/reactive/ReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
